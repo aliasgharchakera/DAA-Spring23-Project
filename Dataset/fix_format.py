9430820f03f73f7e27c1dbf3e5e8ed4be6beeed6
Syed M. Abbas Haider Taqvi <abbastaqvi81@gmail.com>
2023-04-24 13:44:14 +0500

    fix format of files, add folders

---
+++ Dataset/fix_format.py
@@ -0,0 +1,13 @@
+# Append the optimum value to the beginning of each file in the large_scale
+# folder
+
+with open('filenames.txt') as f:
+    filenames = f.readlines()
+
+for file in filenames:
+    with open('large_scale_optimum/' + file.strip()) as f:
+        optimum = f.readlines()[0]
+    with open('large_scale/' + file.strip(), 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(optimum.strip() + '\n' + content)
