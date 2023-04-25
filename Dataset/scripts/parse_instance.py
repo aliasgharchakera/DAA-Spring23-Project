a59db69dab1809b0f1bf1e446d904d1f274fad0e
Syed M. Abbas Haider Taqvi <abbastaqvi81@gmail.com>
2023-04-25 13:24:20 +0500

    Update parse_instance.py
    made optimum string instead of int

--- Dataset/scripts/parse_instance.py
+++ Dataset/scripts/parse_instance.py
@@ -2,7 +2,9 @@
 def parse_instance(filepath):
     """This function will parse the knapsack problem instance file at the filepath
     and return a list of [optimum, n, wmax, V:[vi|i=0 to n-1], W:[wi|i=0 to n-1]] 
-    with the datatype format [int, int, int, [int] [int]]"""
+    with the datatype format [str, int, int, [int] [int]], optimum is left string since right now
+    there are blank lines on the first line on some files and casting a blank string in int 
+    would cause errors. This is to be dealt in analysis part"""
     with open(filepath, 'r') as f:
         lines = f.readlines()
         optimum = lines[0].strip()
@@ -13,4 +15,4 @@ def parse_instance(filepath):
             vi, wi = line.split()
             V.append(int(vi))
             W.append(int(wi))
-    return [int(optimum), int(n), int(wmax), V, W]
+    return [optimum, int(n), int(wmax), V, W]
