fee9a3ea3752639ae42946d841f52191a183c244
Syed M. Abbas Haider Taqvi <abbastaqvi81@gmail.com>
2023-04-25 13:12:39 +0500

    Update knapsack_dp.py

--- Algorithms/knapsack_dp.py
+++ Algorithms/knapsack_dp.py
@@ -2,20 +2,19 @@
 # [GeekforGeeks](https://www.geeksforgeeks.org/python-program-for-dynamic-programming-set-10-0-1-knapsack-problem/)
 
 
-def knapsack_dp(W, wt, val, n):
+def knapsack_dp(wmax, W, V, n):
     ''' A Dynamic Programming based Python Program for 0-1 Knapsack problem Returns
-    the maximum value that can be put in a knapsack of capacity W '''
-    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+    the maximum value that can be put in a knapsack of capacity wmax '''
+    K = [[0 for x in range(wmax + 1)] for x in range(n + 1)]
 
     # Build table K[][] in bottom up manner
     for i in range(n + 1):
-        for w in range(W + 1):
-            if i == 0 or w == 0:
-                K[i][w] = 0
-            elif wt[i-1] <= w:
-                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w])
+        for wi in range(wmax + 1):
+            if i == 0 or wi == 0:
+                K[i][wi] = 0
+            elif W[i-1] <= wi:
+                K[i][wi] = max(V[i-1] + K[i-1][wi-W[i-1]],  K[i-1][wi])
             else:
-                K[i][w] = K[i-1][w]
+                K[i][wi] = K[i-1][wi]
 
-    return K[n][W]
-
+    return K[n][wmax]
