9ca845016a870ced2fd92ae76e92419dd246600a
Haider Abbas <73271899+haider-06418@users.noreply.github.com>
2023-04-27 10:12:19 +0500

    Plotting added

--- Algorithms/knapsack_dp.py
+++ Algorithms/knapsack_dp.py
@@ -90,7 +90,7 @@ def vlargen(plotting_instances):
     # plt.show()
 
 def wmax(plotting_instances):
-    categories = ["very_large_wmax", "very_large_valued_W"]
+    categories = ["very_large_wmax"]
     instances = 100
     iterations = 1
     avg_score = list(); avg_time = list()
@@ -132,19 +132,65 @@ def wmax(plotting_instances):
             f.write(f"{avg_score[i]}, {avg_time[i]}\n")
     
     plotting_instances.append((c_list, time_list))
-    
 
-def main(plotting_instances):
+def vlargev(plotting_instances):
+    categories = ["very_large_valued_V"]
+    instances = 100
+    iterations = 1
+    avg_score = list(); avg_time = list()
+    n_lst = list(); c_lst = list()
+    for c in categories:
+        for i in range(instances):
+            score = list()
+            timelst = list()
+            
+            num = "0"*(4-len(str(i)))
+            location = f"../Dataset/{c}_group/instance_{num}{i}.txt"
+            items = list(); W = list(); V = list()
+            with open(location) as f:
+                lines = f.readlines()
+                num, capacity = lines[0].split()
+                num, capacity = int(num), int(capacity)
+                for i in lines[1::]:
+                    value, weight = i.split()
+                    items.append((int(value), int(weight)))
+                    W.append(int(weight)); V.append(int(value))
+                    
+            for i in range(iterations):
+                with timer("func", timelst):
+                    max = knapsack_dp(W, V, num, capacity)
+                score.append(max)
+            n_lst.append(num)
+            c_lst.append(capacity)
+            avg_score.append(statistics.mean(score))
+            avg_time.append(statistics.mean(timelst))
+            c_t = list(zip(c_lst, avg_time))
+            c_t.sort(key=lambda x: x[0])
+            # n_t is a list of tuples of (n, time), I want two separate lists of n and time
+            c_list, time_list = zip(*c_t)
+    with open('analysis3.csv', 'w') as f:
+        f.write("score, time\n")
+        for i in range(instances*len(categories)):
+            if i % instances == 0:
+                f.write(f"{categories[i//instances]}\n")
+            f.write(f"{avg_score[i]}, {avg_time[i]}\n")
+    
+    plotting_instances.append((c_list, time_list))
+
+
+def dpplot(plotting_instances):
+    # plotting_instances = []
     vlargen(plotting_instances)
-    wmax(plotting_instances)
+    # wmax(plotting_instances)
+    # vlargev()
     # print(plotting_instances)
     for data in plotting_instances:
         plt.plot(data[0], data[1], color='blue', label="")
-    plt.xlabel('n')
+    # plt.xlabel('n')
     plt.ylabel('Time (seconds)')
     plt.legend()
     plt.title('Knapsack - DP')
     plt.show()
 
 plotting_instances = []
-main(plotting_instances)
+# dplot(plotting_instances)
